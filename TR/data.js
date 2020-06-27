6d9c29287c208ee72686aa30af50da1f736b814f
Gina Rasicci <31525899+GRasicci@users.noreply.github.com>
2020-06-26 21:47:19 -0400

    Add files via upload

---
+++ TR/data.js
@@ -0,0 +1,157 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-pano-01b-8k",
+      "name": "360 pano 01b 8K",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.0032855055873195482,
+        "pitch": 0.11738336528057225,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9092466316938665,
+          "pitch": -0.059417878434693705,
+          "rotation": 0,
+          "target": "1-360-pano-03-8k"
+        },
+        {
+          "yaw": 0.9319698807851609,
+          "pitch": -0.03694959475631343,
+          "rotation": 0,
+          "target": "2-360-pano-02-8k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.04898374712324305,
+          "pitch": 0.5115749823453282,
+          "title": "Arthrex Downing Concept A",
+          "text": "<a href=\"https://www.arthrex.com/\">https://www.arthrex.com/</a>"
+        },
+        {
+          "yaw": -0.30042752369666026,
+          "pitch": -0.2525962671862647,
+          "title": "Foot &amp; Ankle Video",
+          "text": "<a href=\"https://www.youtube.com/watch?v=GFbUyQuTz88\">https://www.youtube.com/watch?v=GFbUyQuTz88</a>"
+        }
+      ]
+    },
+    {
+      "id": "1-360-pano-03-8k",
+      "name": "360 pano 03 8K",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.0690630144374218,
+        "pitch": 0.008552052432509782,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07842081036856996,
+          "pitch": 0.3167341939951491,
+          "rotation": 0,
+          "target": "0-360-pano-01b-8k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-360-pano-02-8k",
+      "name": "360 pano 02 8K",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1549119190807957,
+          "pitch": 0.30825028760930806,
+          "rotation": 0,
+          "target": "0-360-pano-01b-8k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.128321454499015,
+          "pitch": 0.5010933227554766,
+          "title": "Typical station",
+          "text": ""
+        },
+        {
+          "yaw": -0.02034411281989179,
+          "pitch": 0.410952569329055,
+          "title": "Sample Wall",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Arthrex Downing A",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
